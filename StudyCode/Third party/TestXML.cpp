124205d78f02043cfb68c52269fb716fc01b43c4
HomePC <26021153@qq.com>
2021-07-24 15:17:33 +0800

    add tinyxml2 writexml

--- StudyCode/Third party/TestXML.cpp
+++ StudyCode/Third party/TestXML.cpp
@@ -19,13 +19,16 @@ using namespace std;
 #include "rapidxml-1.13.0\rapidxml_print.hpp"
 using namespace rapidxml;
 
+#include "tinyxml2/tinyxml2.h"
+using namespace tinyxml2;
+
 // 对外接口
 void TestXML::BeginTest()
 {
 	cout << __FILE__ << "  " << __FUNCTION__ << endl;
 
 	// rapidxml写文件
-	if (0)
+	if (1)
 	{
 		xml_document<> doc;
 
@@ -123,7 +126,84 @@ void TestXML::BeginTest()
 	}
 
 	// tinyxml2 写文件
+	if (1)
+	{
+		const char* declaration = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
+		XMLDocument doc;
+		if (doc.Parse(declaration) != 0) 
+		{
+			return;
+		}
+
+		tinyxml2::XMLComment* comment = doc.NewComment("this is a xml test file");
+		doc.InsertEndChild(comment);
+
+		tinyxml2::XMLElement* root = doc.NewElement("Root");
+		doc.InsertEndChild(root);
+
+		tinyxml2::XMLElement* user = doc.NewElement("User");
+		user->SetAttribute("Name", "fengbingchun");
+		root->InsertEndChild(user);
+
+		tinyxml2::XMLElement* blog = doc.NewElement("Blog");
+		tinyxml2::XMLText* text1 = doc.NewText("CSDN");
+		blog->InsertEndChild(text1);
+		user->InsertEndChild(blog);
+
+		tinyxml2::XMLElement* code = doc.NewElement("Code");
+		tinyxml2::XMLText* text2 = doc.NewText("GitHub");
+		code->InsertEndChild(text2);
+		user->InsertEndChild(code);
+
+		// Blog
+		tinyxml2::XMLElement* blog2 = doc.NewElement("Blog");
+		blog2->SetAttribute("Name", "CSDN");
+		root->InsertEndChild(blog2);
+
+		tinyxml2::XMLElement* addr = doc.NewElement("Address");
+		tinyxml2::XMLText* text3 = doc.NewText("https://blog.csdn.net/fengbingchun");
+		addr->InsertEndChild(text3);
+		blog2->InsertEndChild(addr);
+
+		tinyxml2::XMLElement* id = doc.NewElement("ID");
+		tinyxml2::XMLText* text4 = doc.NewText("fengbingchun");
+		id->InsertEndChild(text4);
+		blog2->InsertEndChild(id);
+
+		// Code
+		tinyxml2::XMLElement* code2 = doc.NewElement("Code");
+		code2->SetAttribute("Name", "GitHub");
+		root->InsertEndChild(code2);
+
+		tinyxml2::XMLElement* addr2 = doc.NewElement("Address");
+		tinyxml2::XMLText* text5 = doc.NewText("https://github.com//fengbingchun");
+		addr2->InsertEndChild(text5);
+		code2->InsertEndChild(addr2);
+
+		tinyxml2::XMLElement* repositories = doc.NewElement("Repositories");
+		tinyxml2::XMLText* text6 = doc.NewText("27");
+		repositories->InsertEndChild(text6);
+		code2->InsertEndChild(repositories);
+
+		// 保存文件
+		doc.SaveFile("tinyTest.xml");
+		doc.Clear();
+	}
 
 	// tinyxml2 读文件
+	if (1)
+	{
+		XMLDocument doc;
+		auto ret = doc.LoadFile("tinyTest.xml");
+		if (ret != XML_SUCCESS)
+		{
+			cout << "LoadFile error = " << ret << endl;
+		}
+
+		auto element = doc.FirstChildElement();
+
+		doc.Clear();
+	}
 
 }
