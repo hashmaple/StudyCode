cd3eddef5baa7fa1e43bc88af9c5be09c2d55684
26021153@qq.com <26021153@qq.com>
2022-01-11 17:22:02 +0800

    模板学习提交

--- StudyCode/CPP language/C++ Templates/C++ Templates.cpp
+++ StudyCode/CPP language/C++ Templates/C++ Templates.cpp
@@ -40,15 +40,19 @@ inline const char*  rt_max(int const& a, int const& b)
 	return "nonsupport int vs int";
 }
 
+// 手动实例化: 显式实例化指示符（explicit instantiation directive）
+template void nonref<int>(int);
+template void nonref<double>(double);
+
 // 对外接口
 void TestTemplates::BeginTest()
 {
 	cout << __FILE__ << "  " << __FUNCTION__ << endl;
 
-	// chapter 2 模板函数(参数,演绎和显式指定等) 
+	// 2 模板函数(参数,演绎和显式指定等) 
 	if (0)
 	{
-		cout << "chapter 2 模板函数(参数,演绎和显式指定等) " << endl;
+		cout << " 2 模板函数(参数,演绎和显式指定等) " << endl;
 
 		// 明确使用::调用全局的max函数,不然会调用STL中的std::max()
 		cout << "::max(1,3) = " << ::max(1, 3) << endl;
@@ -88,10 +92,10 @@ void TestTemplates::BeginTest()
 		*/
 	}
 
-	// chapter 3 类模板
+	// 3 类模板
 	if (0)
 	{
-		cout << "chapter 3 类模板 " << endl;
+		cout << "3 类模板 " << endl;
 
 		// 使用模板容器
 		cout << "模板容器 MyStack<int> stack" << endl;
@@ -167,10 +171,10 @@ void TestTemplates::BeginTest()
 		*/
 	}
 
-	// chapter 4 非类型模板参数
+	// 4 非类型模板参数
 	if (0)
 	{
-		cout << "chapter 4 非类型模板参数 " << endl;
+		cout << "4 非类型模板参数 " << endl;
 		FixedStack<int, 20> intFStack;
 
 		try
@@ -198,10 +202,69 @@ void TestTemplates::BeginTest()
 		内部链接对象（例如string）作为实参。*/
 	}
 
-	// chapter 5 基础性技巧
-	if (1)
+	// 5 基础性技巧
+	if (0)
 	{
-		cout << "chapter 4 非类型模板参数 " << endl;
+		cout << "5 基础性技巧 " << endl;
 
+		// 打印容器
+		vector<int> vct{ 1, 2, 3, 4, 5 };
+		PrintSTLContainer(vct);
+
+		// 模板子类调用基类
+		Derived<int> derived;
+		derived.sub_call();
+
+		// 测试零初始化情况
+		auto y = addValue<int, 10>(100);
+		cout << "addValue<int, 10>(100): " << y << endl;
+
+		// 类型为字符数组
+		ref("hello");
+		// 类型退化到字符指针
+		nonref("hello");
+
+		/*
+		•如果要访问依赖于模板参数的类型名称，你应该在类型名称前添加关键字typename。
+		•嵌套类和成员函数也可以是模板。
+		•赋值运算符的模板版本并没有取代缺省赋值运算符。
+		•类模板也可以作为模板参数，我们称之为模板的模板参数。
+		•模板的模板实参必须精确地匹配。匹配时并不会考虑“模板的模板实参”的缺省模板实参（如std::deque的allocator）。
+		•通过显式调用缺省构造函数，可以确保模板的变量和成员都已经用一个缺省值完成初始化，这种方法对内建类型的变量和成员也适用。
+		•对于字符串，在实参演绎过程中，当且仅当参数不是引用时，才会出现数组到指针（array-to-pointer）的类型转换（称为decay）。*/
+	}
+
+	// 6 模板细节和模板术语
+	if (0)
+	{
+		cout << " 6 模板细节和模板术语" << endl;
+
+		// 我们把模板的定义也包含在声明模板的头文件里面，即让定义和声明都位于同一个头文件中。
+
+		// 调用手动实例化的模板函数
+		nonref(1);
+		nonref(1.1f);
+		
+		// 有一种尝试提高C++编译器诊断信息的技术，它主要是通过在高层模板插入哑代码来实现的，
+		// 我们可以参考Jeremy Siek的Concept Check Library（见[BCCL]）
+		//——它是Boost库的一部分（见[Boost]）。
+
+		// 类模板(class template)：该类是一个模板
+		// 函数模板(function template) 该函数是一个模板
+
+		// 声明（declaration) 是一种 C++构造（construct），
+		// 引入（或重新引入）一个名称到某个 C++作用域中。
+
+		// 定义 （definition) 必须提供一对花括号内部的实体。
+
+		// 和全局变量与静态数据成员一样，
+		// 在整个程序中，非内联函数和成员函数只能被定义一次。
+
+		// 模板参数是指： T array[N];
+		// 位于模板声明或定义内部，关键字template后面所列举的名称（譬如 T, N）。
+
+		// 模板实参是指：ArrayInClass<double,10>
+		// 用来替换模板参数的各个对象（如 double,10）。
+		// 和模板参数不同的是，模板实参可以有不局限于“标识符名称”（就是有多种类型或值）。
 	}
 }
