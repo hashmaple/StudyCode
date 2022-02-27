00c1c7a89cc566ef5c28753af051b0c52617b594
HomePC <26021153@qq.com>
2022-02-27 10:39:02 +0800

    class with pointer member 的代码

--- StudyCode/CPP language/OOP/OOPClass.cpp
+++ StudyCode/CPP language/OOP/OOPClass.cpp
@@ -11,10 +11,17 @@
 #include <iostream>		// 优先系统目录
 using namespace std;
 
-// 支持complex的输出
+// 支持complex的输出(pass by reference, return by reference)
 ostream& operator << (ostream& os, const complex& x)
 {
+	// 若不返回 os, 则无法支持  cout << c1 << c2;
 	return os << '(' << real(x) << ',' << imag(x) << ')';
+}
+
+// 支持String的输出
+ostream& operator << (ostream& os, const String& str)
+{
+	return os << str.get_c_str();
 }
 
 // 对外接口
@@ -32,11 +39,60 @@ void OOPClass::BeginTest()
 
 		cout << c1 << endl;
 		cout << c2 << endl;
+		cout << c1 << c2 << endl;
 
 		cout << c1 + c2 << endl;
+		cout << (c1 == c2) << endl;
 
+		// 转换为: complex:: .operator +=(this, c2)
 		cout << (c1 += c2) << endl;
 
 		cout << (5 + c2) << endl;
+
+		// new操作分解
+		complex* p = new complex(1, 2);
+		delete p;
+		p = nullptr;
+
+		// new complex 编译器转换为以下代码
+		void* mem = operator new(sizeof(complex)); // 分配内存(malloc(n))
+		p = static_cast<complex*>(mem);	// 类型转换
+		p->complex::complex(1, 2);	// 构造函数
+
+		cout << "complex(1, 2) *p = " << *p << endl;
+
+		p->complex::~complex(); // 执行析构函数
+
+		cout << "p->complex::~complex()  *p = " << *p << endl;
+
+		operator delete(p); // 释放内存
+
+		cout << "delete(p) danger!! *p = " << *p << endl;
+	}
+
+	// String 相关测试
+	if (1)
+	{
+		cout << "String 相关测试  " << endl;
+
+		cout << "String("") = " << String(0) << endl;
+
+
+		String s1("s1");
+		String s2("s2");
+
+		cout << "s1 s2 = " << s1 << " " << s2<< endl;
+
+		String s3(s2);
+		cout << "s3(s2) s3=" << s3 << endl;
+
+		s3 = s1;
+		cout << "s3 = s1 s3=" << s3 << endl;
+
+		// 不可漏写[] 影响元素的析构调用
+		String* p = new String[3];
+		std::cout << "new String[3]" << std::endl;
+		delete[] p;
+		std::cout << "delete p[] end" << std::endl;
 	}
 }
--- StudyCode/CPP language/OOP/OOPClass.h
+++ StudyCode/CPP language/OOP/OOPClass.h
@@ -29,35 +29,41 @@ class complex;
 complex& __doapl(complex* ths, const complex& r);
 complex& __doami(complex* ths, const complex& r);
 
-// 类声明
+// 类声明 without pionter class
 class complex
 {
 public:
-	// 构造函数 + 初始化列表
+	// 构造函数 + 初值列,初始列(区别于赋值)
 	complex(double r = 0, double i = 0): re(r), im(i) {}
+	
+	// 析构函数
+	~complex() { re = 0; im = 0; }
 
-	// 操作符重载
+	// 操作符重载 (return by reference)
 	complex& operator += (const complex&);
 	complex& operator -= (const complex&);
 
-	// const描述此方法不会修改内部数据 const对象也可以调用
+	// const常量成员函数 此方法不会修改内部数据 const对象也可以调用
 	double real() const { return re; }
+	// 在class body内直接定义,自动成为inline的候选人
 	double imag() const { return im; }
 
 private:
 	double re, im;
 
-	// 友函数(函数内可以访问private member)
+	// 友元(函数内可以访问private member)
 	friend complex& __doapl(complex*, const complex&);
 	friend complex& __doami(complex*, const complex&);
 };
 
-// 类定义(inline建议, 最终按编译器为准)
+// 在body外的类定义(inline最终按编译器为准)
 inline complex& __doapl(complex* ths, const complex& r)
 {
-	// 返回类型为 complex&, 不可返回临时对象~
+	// 因为为友元,自由取得friend的private成员
 	ths->re += r.re;
 	ths->im += r.im;
+
+	// 返回类型为 complex&, 不可返回临时对象~
 	return *ths;
 }
 
@@ -67,6 +73,7 @@ inline complex& complex::operator += (const complex& r)
 	return __doapl(this, r);
 }
 
+// 第一个参数会改变,第二个参数不会改变 返回引用
 inline complex& __doami(complex* ths, const complex& r)
 {
 	// 返回类型为 complex&, 不可返回临时对象~
@@ -86,10 +93,87 @@ inline double imag(const complex& x) { return x.imag(); }
 
 inline double real(const complex& x){ return x.real(); }
 
-// 不可返回complex&, 因为结果为临时对象
+// 不可返回complex&, 因为结果为临时对象 local object
 inline complex operator + (const complex& x, const complex& y)
 {
 	return complex(real(x) + real(y), imag(x) + imag(y));
+}
+
+// 操作符重载, 非成员函数
+inline bool operator == (const complex& x, const complex& y)
+{
+	return real(x) == real(y) && imag(x) == imag(y);
+}
+
+
+// 类声明 with pionter class
+class String
+{
+	// Big Three 三个特殊函数
+public:
+	// class with pointer 必须有 copy ctor 和 op=
+	String(const char* cstr = 0);
+	String(const String& str); // 拷贝构造函数
+	String& operator=(const String& str); // 拷贝赋值函数
+	// 开启了堆内存,就必须析构时释放
+	~String();
+	
+	char* get_c_str() const	{ return m_data; }
+
+private:
+	char* m_data;
+};
+
+// String类定义
+#include <cstring>
+inline String::String(const char* cstr)
+{
+	// 参数保护
+	if (cstr)
+	{
+		// heap堆上开空间,复制内容
+		m_data = new char[strlen(cstr) + 1];
+		strcpy_s(m_data, strlen(cstr) + 1, cstr);
+	}
+	else
+	{
+		// 确保构造成功,空内容的字符串
+		m_data = new char[1];
+		*m_data = '\0';
+	}
+}
+
+inline String::String(const String& str)
+{
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy_s(m_data, strlen(str.m_data) + 1, str.m_data);
+}
+
+inline String& String::operator =(const String& str)
+{
+	// 检测自我赋值 self assignment
+	if (this == &str)
+	{
+		return *this;
+	}
+
+	delete[] m_data;
+
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy_s(m_data, strlen(str.m_data) + 1, str.m_data);
+	return *this;
+}
+
+#include <iostream>
+
+inline String::~String()
+{
+	if (m_data)
+	{
+		delete[] m_data;
+	}
+
+	std::cout << "~String" << std::endl;
 }
 
 #endif // OOPClass_h__
